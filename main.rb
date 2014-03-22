f62dd2ff0eedfca086f833d8dd710534512a51a1
Nick Stewart <nicholas4@gmail.com>
2014-03-22 19:39:26 +0000

    Add class Expression for namespacing

--- main.rb
+++ main.rb
@@ -1,49 +1,51 @@
 Dir[File.dirname(__FILE__) + '/lib/**/*.rb'].each {|file| require file }
 
-class TernaryExpression
-  def initialize(variable_terminals, depth)
-    @operator  = get_operator
-    @operand_1 = ::Expression::OperandCreator.create(variable_terminals, depth)
-    @operand_2 = ::Expression::OperandCreator.create(variable_terminals, depth)
-    @conditional = ConditionalExpression.new(variable_terminals, depth)
+class Expression
+  class TernaryExpression
+    def initialize(variable_terminals, depth)
+      @operator  = get_operator
+      @operand_1 = ::Expression::OperandCreator.create(variable_terminals, depth)
+      @operand_2 = ::Expression::OperandCreator.create(variable_terminals, depth)
+      @conditional = ConditionalExpression.new(variable_terminals, depth)
+    end
+
+    def get_operator
+    end
+
+    def to_s
+      "( " + @conditional.to_s + " ? " + @operand_1.to_s + " : " +  @operand_2.to_s + " )"
+    end
   end
 
-  def get_operator
+  class BinaryExpression < Expression::Base
+    def initialize(variable_terminals, depth)
+      super
+      @operand_2 = ::Expression::OperandCreator.create(variable_terminals, depth)
+    end
+
+    def get_operator
+      ['+', '-', '*', '/', '**'].sample
+    end
+
+    def to_s
+      "( " + @operand_1.to_s + " " + @operator.to_s + " " +  @operand_2.to_s + " )"
+    end
   end
 
-  def to_s
-    "( " + @conditional.to_s + " ? " + @operand_1.to_s + " : " +  @operand_2.to_s + " )"
-  end
-end
-
-class BinaryExpression < Expression::Base
-  def initialize(variable_terminals, depth)
-    super
-    @operand_2 = ::Expression::OperandCreator.create(variable_terminals, depth)
+  class ConditionalExpression < BinaryExpression
+    def get_operator
+      ['<', '<=', '>', '>='].sample
+    end
   end
 
-  def get_operator
-    ['+', '-', '*', '/', '**'].sample
-  end
+  class UnaryExpression < Expression::Base
+    def get_operator
+      '-'
+    end
 
-  def to_s
-    "( " + @operand_1.to_s + " " + @operator.to_s + " " +  @operand_2.to_s + " )"
-  end
-end
-
-class ConditionalExpression < BinaryExpression
-  def get_operator
-    ['<', '<=', '>', '>='].sample
-  end
-end
-
-class UnaryExpression < Expression::Base
-  def get_operator
-    '-'
-  end
-
-  def to_s
-    "( " + @operator.to_s + " " + @operand_1.to_s + " )"
+    def to_s
+      "( " + @operator.to_s + " " + @operand_1.to_s + " )"
+    end
   end
 end
 
